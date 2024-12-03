863f5b77d297772ebd70fb78d547d96373453e07
dkn1fe <dknifeb@mail.ru>
2024-12-03 22:21:15 +0300

    change config webpack

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -38,13 +38,6 @@ const webpackConfig = merge(commonConfig, {
             maxInitialRequests: Infinity,
             minSize: 0,
             cacheGroups: {
-                vendor: {
-                    test: /[\\/]node_modules[\\/]/,
-                    name (module) {
-                        const packageName = module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1];
-                        return `npm.${packageName.replace('@', '')}`;
-                    }
-                },
                 styles: {
                     test: /\.css$/,
                     name: 'styles',
